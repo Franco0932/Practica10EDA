66925f634354da8a710f1bfa503cc8f64968b4eb
Franco0932 <54223581+Franco0932@users.noreply.github.com>
2020-04-19 15:02:27 -0500

    Add files via upload

---
+++ biblioteca1.py
@@ -0,0 +1,15 @@
+import math
+x = math.cos(math.pi)
+print (x)
+
+print("*******************************************************************************")
+
+from math import *
+x = cos(pi)
+print(x)
+
+print("*******************************************************************************")
+
+from math import cos, pi
+x = cos(pi)
+print (x)
